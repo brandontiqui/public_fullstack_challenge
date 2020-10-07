96a626b97d7c14fda240d22aa74be442c27b1c71
Brandon <btiqui@gmail.com>
2020-10-07 01:02:16 -0700

    Search for farms by name

--- farm-app-fullstack/client/src/components/FarmApp.jsx
+++ farm-app-fullstack/client/src/components/FarmApp.jsx
@@ -4,6 +4,11 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import { TextField } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { withStyles } from '@material-ui/core/styles';
 
@@ -20,6 +25,7 @@ class FarmApp extends React.Component {
     };
 
     this.handleSelectFarm = this.handleSelectFarm.bind(this);
+    this.handleFarmNameFilter = this.handleFarmNameFilter.bind(this);
   }
 
   componentDidMount() {
@@ -85,6 +91,26 @@ class FarmApp extends React.Component {
     };
   }
 
+  handleFarmNameFilter(e) {
+    console.log('e.target.value', e.target.value);
+    const searchText = e.target.value.toLowerCase();
+    console.log('searchText', searchText);
+    const { farmList } = this.state;
+    farmList.forEach(farm => {
+      if (farm.name.toLowerCase().indexOf(searchText) === -1) {
+        farm.display = false;
+      } else {
+        farm.display = true;
+      }
+      // no filtered search text
+      if (searchText === '') {
+        farm.display = true;
+      }
+    });
+    console.log('farmList', farmList);
+    this.setState({ farmList });
+  }
+
   render() {
     const { classes } = this.props;
     const { farmList, selectedFarmId, farmData } = this.state;
@@ -109,9 +135,16 @@ class FarmApp extends React.Component {
         >
           <Toolbar />
           <div className={classes.drawerContainer}>
-            {farmList.map(farm => (
-              <div key={farm.farmId} onClick={this.handleSelectFarm(farm.farmId)}>{farm.name}</div>
-            ))}
+            <TextField onChange={this.handleFarmNameFilter} />
+            <List>
+              {farmList
+                .filter(farm => farm.display)
+                .map(farm => (
+                <ListItem button key={farm.farmId} onClick={this.handleSelectFarm(farm.farmId)}>
+                  <ListItemText primary={farm.name} />
+                </ListItem>
+              ))}
+            </List>
           </div>
         </Drawer>
         <main className={classes.content}>
